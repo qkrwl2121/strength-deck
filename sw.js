552bdd2fab8cf2f092840ac75c2a9f7fe7ffbcf2
qkrwl2121 <qkrwl2121@gmail.com>
2026-06-22 18:39:02 +0900

    Increase form control height

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "strength-deck-v7";
+const CACHE_NAME = "strength-deck-v8";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
